ae1d85f82bf673dc11728ed59f8d117577c91389
Marcos9868 <marcos_m_ferreira@outlook.com>
2023-11-28 18:41:56 -0300

    Remove: product data, Feat: DataContext on controller

--- Server/Controllers/ProductController.cs
+++ Server/Controllers/ProductController.cs
@@ -1,3 +1,4 @@
+using BlazorEcommerce.Server.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlazorEcommerce.Server.Controllers
@@ -6,33 +7,12 @@ namespace BlazorEcommerce.Server.Controllers
     [Route("api/[controller]")]
     public class ProductController : ControllerBase
     {
-        private static List<Product> Products = new List<Product>
+        private readonly DataContext _context;
+
+        public ProductController(DataContext context)
         {
-            new Product
-            {
-                IdProduct = 1,
-                Title = "Black Cavendish Tobacco",
-                Description = "A smoothy and sweetness tobacco",
-                Image_Url = "",
-                Price = 9.99m
-            },
-            new Product
-            {
-                IdProduct = 2,
-                Title = "1984 - George Orhwell",
-                Description = "This books tells about comunism destroy our world",
-                Image_Url = "",
-                Price = 19.99m
-            },
-            new Product
-            {
-                IdProduct = 3,
-                Title = "SmartWatch",
-                Description = "SmartWatch to monitore your heartbeats",
-                Image_Url = "",
-                Price = 29.99m
-            }
-        };
+            _context = context;    
+        }
 
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Product))]
@@ -40,6 +20,7 @@ namespace BlazorEcommerce.Server.Controllers
         [Route("/products")]
         public async Task<IActionResult> GetProducts()
         {
+            var Products = await _context.Products.ToListAsync();
             if (Products is null)
                 return BadRequest("Unable to list products");
                 
