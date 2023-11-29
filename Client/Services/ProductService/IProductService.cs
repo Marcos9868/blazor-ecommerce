a6aae907901e8f23f8bcc8e6b70fc3cf05fc0fe0
Marcos9868 <marcos_m_ferreira@outlook.com>
2023-11-28 21:53:16 -0300

    Feat: Map of product services on client side

---
+++ Client/Services/ProductService/IProductService.cs
@@ -0,0 +1,8 @@
+namespace BlazorEcommerce.Client.Services.ProductService
+{
+    public interface IProductService
+    {
+        List<Product> Products { get; set; }
+        Task GetProducts();
+    }
+}
